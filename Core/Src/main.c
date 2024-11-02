c6a395f07fbe5516880f16e5d67af38b6eedc91d
Douglas P. Fields, Jr. <symbolics@lisp.engineer>
2024-11-02 15:13:29 -0400

    Found some documentation on SDRAM initialization for STM32 but still not working.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -256,6 +256,44 @@ static void MX_FMC_Init(void)
 
   /* USER CODE BEGIN FMC_Init 2 */
 
+  // Code from: https://community.st.com/t5/stm32-mcus/how-to-set-up-the-fmc-peripheral-to-interface-with-the-sdram/ta-p/49457
+  // See section 2.6 SDRAM Initialization sequence
+
+  FMC_SDRAM_CommandTypeDef Command;
+  /* Step 1 and Step 2 already done in HAL_SDRAM_Init() */
+  /* Step 3: Configure a clock configuration enable command */
+  Command.CommandMode            = FMC_SDRAM_CMD_CLK_ENABLE; /* Set MODE bits to "001" */
+  Command.CommandTarget          = FMC_SDRAM_CMD_TARGET_BANK1; /* configure the Target Bank bits */
+  Command.AutoRefreshNumber      = 1;
+  Command.ModeRegisterDefinition = 0;
+  HAL_SDRAM_SendCommand(&hsdram1, &Command, 0xfff);
+  HAL_Delay(1); /* Step 4: Insert 100 us minimum delay - Min HAL Delay is 1ms */
+  /* Step 5: Configure a PALL (precharge all) command */
+  Command.CommandMode            = FMC_SDRAM_CMD_PALL; /* Set MODE bits to "010" */
+  HAL_SDRAM_SendCommand(&hsdram1, &Command, 0xfff);
+  /* Step 6: Configure an Auto Refresh command */
+  Command.CommandMode            = FMC_SDRAM_CMD_AUTOREFRESH_MODE; /* Set MODE bits to "011" */
+  Command.AutoRefreshNumber      = 2;
+  HAL_SDRAM_SendCommand(&hsdram1, &Command, 0xfff);
+  /* Step 7: Program the external memory mode register */
+  Command.CommandMode            = FMC_SDRAM_CMD_LOAD_MODE;/*set the MODE bits to "100" */
+  Command.ModeRegisterDefinition =  (uint32_t)0 | 0<<3 | 2<<4 | 0<<7 | 1<<9;
+  HAL_SDRAM_SendCommand(&hsdram1, &Command, 0xfff);
+  /* Step 8: Set the refresh rate counter - refer to section SDRAM refresh timer register in RM0455 */
+  /* Per our SDRAM:
+   *
+   * self refresh time = 64ms
+   * number of rows = 8192
+   * SDRAM Clock = 84MHz (168MHz / 2)
+   *
+   * COUNT = [(SDRAM self refresh time / number of row) x  SDRAM CLK] – 20
+   *       = [(64ms / 8192) * 84MHz] - 20
+   *       = 656.25 - 20
+   *       = 646.25
+   *       = ~646
+   */
+  HAL_SDRAM_ProgramRefreshRate(&hsdram1, 646);
+
   /* USER CODE END FMC_Init 2 */
 }
 
